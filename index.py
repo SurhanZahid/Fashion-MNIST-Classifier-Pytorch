a0fd4ee6ea86d07f4759e6900d0a962693e3c295
SurhanZahid <surhanzahid@gmail.com>
2019-01-19 02:19:44 +0500

    Update 1
    First update with all the code

---
+++ index.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 18:25:14 2019
+
+@author: Surhan
+"""
+
+# Import Files 
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from collections import OrderedDict
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                             ])
+# Download and load the training data
+trainset = datasets.FashionMNIST('F_MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Download and load the test data
+testset = datasets.FashionMNIST('F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
+def imshow(image, ax=None, title=None, normalize=True):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    image = image.numpy().transpose((1, 2, 0))
+
+    if normalize:
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        image = std * image + mean
+        image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.tick_params(axis='both', length=0)
+    ax.set_xticklabels('')
+    ax.set_yticklabels('')
+
+    return ax
+
+def view_classify(img, ps, version="Fashion"):
+    ''' Function for viewing an image and it's predicted classes.
+    '''
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    if version == "MNIST":
+        ax2.set_yticklabels(np.arange(10))
+    elif version == "Fashion":
+        ax2.set_yticklabels(['T-shirt/top',
+                            'Trouser',
+                            'Pullover',
+                            'Dress',
+                            'Coat',
+                            'Sandal',
+                            'Shirt',
+                            'Sneaker',
+                            'Bag',
+                            'Ankle Boot'], size='small');
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+
+#Display the images from the traning data
+image, label = next(iter(trainloader))
+imshow(image[1,:]);
+
+#Building our 
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Defining the layers, 128, 64, 10 units each
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 64)
+        # Output layer, 10 units - one for each digit
+        self.fc3 = nn.Linear(64, 10)
+        
+    def forward(self, x):
+        ''' Forward pass through the network, returns the output logits '''
+        
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.softmax(x, dim=1)
+        
+        return x
+ 
+model = Network()
+
+
+
+# Grab traning data
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# Resize images into a 1D vector, new shape is (batch size, color channels, image pixels) 
+images.resize_(64, 1, 784)
+# or images.resize_(images.shape[0], 1, 784) to not automatically get batch size
+
+# Forward pass through the network
+img_idx = 0
+ps = model.forward(images[img_idx,:])
+
+img = images[img_idx]
+view_classify(img.view(1, 28, 28), ps)
+
+# Hyperparameters for our network
+input_size = 784
+hidden_sizes = [200, 100]
+output_size = 10
+
+
+model = nn.Sequential(OrderedDict([
+                      ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                      ('relu1', nn.ReLU()),
+                      ('fc2', nn.Linear(hidden_sizes[0], hidden_sizes[1])),
+                      ('relu2', nn.ReLU()),
+                      ('output', nn.Linear(hidden_sizes[1], output_size)),
+                      ('relu3', nn.ReLU())]))
+
+    
+# Traning Our Model
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+images, labels = next(iter(trainloader))
+images.resize_(64, 784)
+
+# Clear the gradients, do this because gradients are accumulated
+optimizer.zero_grad()
+
+# Forward pass, then backward pass, then update weights
+output = model.forward(images)
+loss = criterion(output, labels)
+loss.backward()
+#print('Gradient -', model.fc1.weight.grad)
+optimizer.step()
+
+epochs = 5
+print_every = 100
+steps = 0
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in iter(trainloader):
+        steps += 1
+        # Flatten MNIST images into a 784 long vector
+        images.resize_(images.size()[0], 784)
+        
+        optimizer.zero_grad()
+        
+        # Forward and backward passes
+        output = model.forward(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            print("Epoch: {}/{}... ".format(e+1, epochs),
+                  "Loss: {:.4f}".format(running_loss/print_every))
+            
+            running_loss = 0
+
+# Now lets test our with trained data
+images, labels = next(iter(trainloader))
+
+img = images[1].view(1, 784)
+# Turn off gradients to speed up this part
+with torch.no_grad():
+    logits = model.forward(img)
+
+# Output of the network are logits, need to take softmax for probabilities
+ps = F.softmax(logits, dim=1)
+view_classify(img.view(1, 28, 28), ps,version='Fashion')
+
+
+# Now lets test our with testt data
+images, labels = next(iter(testloader))
+
+img = images[1].view(1, 784)
+# Turn off gradients to speed up this part
+with torch.no_grad():
+    logits = model.forward(img)
+
+# Output of the network are logits, need to take softmax for probabilities
+ps = F.softmax(logits, dim=1)
+view_classify(img.view(1, 28, 28), ps,version='Fashion')
